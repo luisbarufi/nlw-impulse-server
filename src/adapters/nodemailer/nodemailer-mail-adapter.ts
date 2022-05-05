c71d955ef6846056abad0e5558b7179b0a507877
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-04 23:34:06 -0300

    stage three completed

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5800cd2b8a1bf3",
+    pass: "fcbb6ba9cc6793"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Luis Barufi <barufi.info@gmail.com>',
+      subject,
+      html: body
+    });
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import { routes } from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hellow world');
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('HTTP server running!');
